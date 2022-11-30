569c1815d810c6059b8f30556dbe087be8cc9dd5
TellM1 <terumi.contact.web@gmail.com>
2022-11-30 20:02:07 +0900

    Initial Commit

---
+++ practice_Swift/practice_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  practice_SwiftApp.swift
+//  practice_Swift
+//
+//  Created by Terumi Kawano on 2022/11/30.
+//
+
+import SwiftUI
+
+@main
+struct practice_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
